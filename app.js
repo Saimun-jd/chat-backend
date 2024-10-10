e58bf462dec0dbea38c7cae7b25bd4d87a8ed5df
Saimun-jd <saimuncoder@gmail.com>
2024-10-10 07:48:49 +0600

    changed email send logic

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import sgMail from "@sendgrid/mail"
 
 const app = express()
 const corsOptions = {
-    origin: ['http://localhost:3000',, 'https://mychatify-react.vercel.app'],
+    origin: ['http://localhost:3000',, 'https://slurpping.onrender.com'],
     credentials:true,
 	optionsSuccessStatus: 200,
     methods: ["POST", "GET"]
