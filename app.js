2222ffa5b1f46582dbf9e8d49b5ae2255220d581
Saimun-jd <saimuncoder@gmail.com>
2024-10-14 23:58:17 +0600

    added a route for image analysis

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/auth.routes.js";
 import { connectToDB } from "./db/connectToMongoDB.js";
 import messageRoutes from "./routes/message.routes.js";
 import friendsRoutes from "./routes/friends.routes.js";
+import oceanRoutes from "./routes/ocean.routes.js";
 import cors from "cors";
 import sgMail from "@sendgrid/mail"
 import {Server} from 'socket.io';
@@ -13,7 +14,7 @@ import {createServer} from 'http';
 const app = express()
 const server = createServer(app);
 const corsOptions = {
-    origin: ['http://localhost:3000',, 'https://slurpping.onrender.com'],
+    origin: ['http://localhost:3000', 'https://slurpping.onrender.com', 'http://localhost:5173'],
     credentials:true,
 	optionsSuccessStatus: 200,
     methods: ["POST", "GET"]
@@ -33,12 +34,29 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/friends", friendsRoutes);
+app.use("/api/ocean", oceanRoutes);
 
 const io = new Server(server, {
     pingTimeout: 6000,
     cors: {
         origin: ['http://localhost:3000', 'https://slurpping.onrender.com']
     }
+})
+// store online users
+let activeUsers = [];
+io.on('connection', (socket) => {
+    socket.on('new-user-add', (newUserId) => {
+        if(!activeUsers.some((user) => user._id === newUserId)) {
+            activeUsers.push({_id: newUserId, socketId: socket.id});
+            console.log("new user connected");
+        }
+        io.emit('get-users', activeUsers);
+    })
+
+    socket.on('disconnect', () => {
+        activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+        io.emit('get-users', activeUsers);
+    })
 })
 
 global.io = io;
