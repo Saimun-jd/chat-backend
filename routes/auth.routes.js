7f14ba94d6c166fd9e136f0dad8c1e174062df5f
Saimun-jd <saimuncoder@gmail.com>
2024-10-18 19:06:55 +0600

    url slight change

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -13,7 +13,7 @@ const router = express.Router();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback",
+    callbackURL: "https://slurpping-api.onrender.com/api/auth/google/callback",
     scope: ['profile', 'email']
   },
   async (accessToken, refreshToken, profile, done) => {
